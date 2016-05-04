7e1fa9446d8597e7aac2aefb1c706910b1fa93d6
Jefferson Reis <jeffersonreis.pro@gmail.com>
2016-05-04 20:46:27 -0300

    - First commit; - Build Web GL with integration of speech recognition;

---
+++ Web GL with Google Speech Recogniton/Assets/Scripts/GoogleSpeechRecognition.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GoogleSpeechRecognition : MonoBehaviour {
+
+    public Text text;
+    private string myResponse = "...";
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = myResponse;
+    }
+
+
+    /// <summary>
+    /// The browser sends the result of speech recognition in this role.
+    /// </summary>
+    /// <param name="result"> result of speech recognition </param>
+    public void GetResponse(string result)
+    {
+        myResponse = result;
+    }
+
+    /// <summary>
+    /// Start speech recognition in the browser
+    /// </summary>
+    public void StartSpeechRecognitionInTheBrowser()
+    {
+        Application.ExternalCall("startButtonFromUnity3D");
+    }
+
+
+
+}
---
+++ Web GL with Google Speech Recogniton/Assets/Scripts/RotationCube.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationCube : MonoBehaviour
+{
+
+    float velocity = 2;
+
+	private float ramdonTime;
+	private Vector3 rotateDir;
+	// Use this for initialization
+	void Start ()
+	{
+		float x, y, z;
+		x = Random.Range (5, 10);
+		y = Random.Range (5, 10);
+		z = Random.Range (5, 10);
+		rotateDir = new Vector3(x,y,z);
+
+		ramdonTime = Random.Range (2, 5);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		ramdonTime -= Time.deltaTime * 1;
+
+		transform.Rotate(rotateDir * Time.deltaTime* velocity);
+
+
+		if (ramdonTime <= 0) {
+			Start ();
+		}
+	}//fecha update
+
+
+
+}
+//fecha a classe
